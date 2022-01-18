29b85d58983ce7b7360d8ef7787541111c26429a
K1unk <klunkamel@mail.ru>
2022-01-18 23:39:14 +0300

    Dockerfile fixed (with pom), Tests fixed (with Converter)

--- src/test/java/TestConverter.java
+++ src/test/java/TestConverter.java
@@ -33,19 +33,44 @@ public class TestConverter {
     }
 
     @Test
-    public void testFToC() {
-        testFC("f\n12\n", "-11.1");
-        testFC("f\n-123.3\n", "-86.3");
-        testFC("f\n0\n", "-17.8");
-        testFC("f\n32\n", "0.0");
+    public void testFahrenheitToCelsius() {
+        Assert.assertEquals(Converter.countTemperatureFahrenheitToCelsius(12), -11.1, 0.05);
+        Assert.assertEquals(Converter.countTemperatureFahrenheitToCelsius(-123.3), -86.3,0.05);
+        Assert.assertEquals(Converter.countTemperatureFahrenheitToCelsius(0), -17.8,0.05);
+        Assert.assertEquals(Converter.countTemperatureFahrenheitToCelsius(32), 0,0.05);
     }
 
     @Test
-    public void testCToF() {
-        testFC("c\n-11.1\n", "12.0");
-        testFC("c\n-86.3\n", "-123.3");
-        testFC("c\n-17.8\n", "0.0");
-        testFC("c\n0\n", "32.0");
+    public void testCelsiusToFahrenheit() {
+        Assert.assertEquals(Converter.countTemperatureCelsiusToFahrenheit(-11.1), 12, 0.05);
+        Assert.assertEquals(Converter.countTemperatureCelsiusToFahrenheit(-86.3), -123.3,0.05);
+        Assert.assertEquals(Converter.countTemperatureCelsiusToFahrenheit(-17.8), 0,0.05);
+        Assert.assertEquals(Converter.countTemperatureCelsiusToFahrenheit(0), 32,0.05);
+    }
+
+    @Test
+    public void testCheckModeCorrectness() {
+        Assert.assertTrue(Converter.checkModeCorrectness("f"));
+        Assert.assertTrue(Converter.checkModeCorrectness("F"));
+        Assert.assertTrue(Converter.checkModeCorrectness("c"));
+        Assert.assertTrue(Converter.checkModeCorrectness("C"));
+        Assert.assertFalse(Converter.checkModeCorrectness("ф"));
+        Assert.assertFalse(Converter.checkModeCorrectness("Ф"));
+        Assert.assertFalse(Converter.checkModeCorrectness("d"));
+        Assert.assertFalse(Converter.checkModeCorrectness("12"));
+    }
+
+    @Test
+    public void testCheckValueCorrectness() {
+        Assert.assertTrue(Converter.checkValueCorrectness("12.2"));
+        Assert.assertTrue(Converter.checkValueCorrectness("-11.3"));
+        Assert.assertTrue(Converter.checkValueCorrectness("0.0"));
+        Assert.assertTrue(Converter.checkValueCorrectness("0"));
+        Assert.assertTrue(Converter.checkValueCorrectness("1"));
+        Assert.assertFalse(Converter.checkValueCorrectness("1."));
+        Assert.assertFalse(Converter.checkValueCorrectness("1.4.4"));
+        Assert.assertFalse(Converter.checkValueCorrectness("asd"));
+        Assert.assertFalse(Converter.checkValueCorrectness("123-1"));
     }
 
     @Test
